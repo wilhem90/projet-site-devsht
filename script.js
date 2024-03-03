c8a75cac68da7c3a50f4766760e002554adae9e8
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2024-03-03 02:03:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const headerContainer = document.querySelector(".header-container")
 window.addEventListener("DOMContentLoaded", () => {
-
     // vamos preencher o header da pagina, chamamos a funcão getComponent, passando os parametro necessario nessa chamada.
     getComponent("./src/components/header/navbar.html", headerContainer)
 
-    async function getComponent(url, element) {
-        try {
-            const response = await fetch(`${url}`)
-            const responseTxt = await response.text()
-            if (response.status === 404) {
-                element.innerHTML = responseTxt
-                return
-            }
-
-            headerContainer.innerHTML = responseTxt
-        } catch (error) {
-            headerContainer.innerHTML = error
-        }
-    }
 })
+
+
+
+async function getComponent(url, element) {
+    try {
+        const response = await fetch(`${url}`)
+        const responseTxt = await response.text()
+        if (response.status === 404) {
+            element.innerHTML = responseTxt
+            return
+        }
+
+        headerContainer.innerHTML = responseTxt
+    } catch (error) {
+        headerContainer.innerHTML = error
+    }
+}
