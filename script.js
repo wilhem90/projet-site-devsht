bcff3b5b7caf74a7e6153ad14a44e16af2843ce9
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2024-03-27 21:58:12 -0300

    refresh

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-const headerContainer = document.querySelector(".header-container")
-const centerContainer = document.querySelector(".center-container")
-
-
-window.addEventListener("DOMContentLoaded", () => {
-
-    // vamos preencher o header da pagina, chamamos a funcão getComponent, passando os parametro necessario nessa chamada.
-    getComponent("./src/components/header/navbar.html", headerContainer)
-    getComponent("./src/components/main/center/center.html", centerContainer)
-
-})
-
-async function getComponent(url, element) {
-    try {
-        const response = await fetch(`${url}`)
-        const responseTxt = await response.text()
-        if (response.status === 404) {
-            element.innerHTML = responseTxt
-            return
-        }
-
-        element.innerHTML = responseTxt
-    } catch (error) {
-        element.innerHTML = error
-    }
+window.onload = function() {
+    const boxPost = document.querySelector(".box-post")
+    getComponents("./components/CreatePost.html", boxPost)
+    
 }
+
+async function getComponents(url, component) {
+    const response = await fetch(url)
+    const responseText = await response.text()
+    component.innerHTML = responseText
+    
+}
+
